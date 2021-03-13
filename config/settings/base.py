701e503571dc326e770d2edc2bbb72f5f674a4fe
tyler <tyler@MacBook-Pro-2.athenian.org>
2021-03-12 22:17:15 -0800

    refactor

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "dorm_launch_django.users.apps.UsersConfig",
+    "dorm_launch_django.shuttles.apps.ShuttlesConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
