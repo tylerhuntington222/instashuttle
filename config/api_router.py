5395ceb75d47d62d699d282ddf6371eca8355a19
tyler <tyler@MacBook-Pro-2.athenian.org>
2021-03-12 22:09:57 -0800

    refactor

--- config/api_router.py
+++ config/api_router.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from rest_framework.routers import DefaultRouter, SimpleRouter
 
 from dorm_launch_django.users.api.views import UserViewSet
+from dorm_launch_django.shuttles.api.views import ShuttleViewSet
 
 if settings.DEBUG:
     router = DefaultRouter()
