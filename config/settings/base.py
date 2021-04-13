f5147b0600911fc655ec6a92b2866981ca90e287
tyler <tyler@MacBook-Pro-2.athenian.org>
2021-04-12 21:29:47 -0700

    Add staff actions to shuttles

--- config/settings/base.py
+++ config/settings/base.py
@@ -97,7 +97,7 @@ AUTHENTICATION_BACKENDS = [
 AUTH_USER_MODEL = "users.User"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "users:redirect"
+LOGIN_REDIRECT_URL = "shuttles:list"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
 
